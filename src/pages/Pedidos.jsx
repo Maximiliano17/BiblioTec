12af1663527cc048e94209bdea7bf3359da7cd95
maximiliano17 <maxidossantosburgos@gmail.com>
2023-11-04 17:24:55 -0300

    subir cmabios

---
+++ src/pages/Pedidos.jsx
@@ -0,0 +1,17 @@
+//Components
+import Header from "../components/Header"
+//Estilos
+import styles from "../modules/Pedidos.module.css"
+function Pedidos() {
+    return (
+      <>
+      {/*Pedidos*/}
+       <Header />
+       <div className={styles.pedidosContent}>
+        
+       </div>
+      </>
+    )
+  } 
+  
+  export default Pedidos
