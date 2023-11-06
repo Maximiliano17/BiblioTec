d1bdc5e24b36315948abcea186d5c343c7346e97
maximiliano17 <maxidossantosburgos@gmail.com>
2023-11-06 09:56:56 -0300

    Suir seccion pedidos modificada

--- src/pages/Pedidos.jsx
+++ src/pages/Pedidos.jsx
@@ -4,11 +4,110 @@ import Header from "../components/Header"
 import styles from "../modules/Pedidos.module.css"
 function Pedidos() {
     return (
-      <>
+      <> 
       {/*Pedidos*/}
        <Header />
        <div className={styles.pedidosContent}>
-        
+        <div className={styles.pedidoFactura}>
+         <div className={styles.encabezado}>
+          <p>Pedido Numero #</p>
+          <p>Fecha:</p>
+          <p>Hora:</p>
+         </div>
+          <div className={styles.contenido}>
+            <p>Profesor:</p>
+            <p>Cruso:</p>
+            <p>Divicion:</p>
+          </div>
+          <div className={styles.librosPedido}>
+
+          </div>
+          <div className={styles.bottones}>
+           <button>Borrar</button>
+           <button>Informar</button>
+           <button>Donwload</button>
+          </div>
+        </div>
+        <div className={styles.pedidoFactura}>
+         <div className={styles.encabezado}>
+          <p>Pedido Numero #</p>
+          <p>Fecha:</p>
+          <p>Hora:</p>
+         </div>
+          <div className={styles.contenido}>
+            <p>Profesor:</p>
+            <p>Cruso:</p>
+            <p>Divicion:</p>
+          </div>
+          <div className={styles.librosPedido}>
+
+          </div>
+          <div className={styles.bottones}>
+           <button>Borrar</button>
+           <button>Informar</button>
+           <button>Donwload</button>
+          </div>
+        </div>
+        <div className={styles.pedidoFactura}>
+         <div className={styles.encabezado}>
+          <p>Pedido Numero #</p>
+          <p>Fecha:</p>
+          <p>Hora:</p>
+         </div>
+          <div className={styles.contenido}>
+            <p>Profesor:</p>
+            <p>Cruso:</p>
+            <p>Divicion:</p>
+          </div>
+          <div className={styles.librosPedido}>
+
+          </div>
+          <div className={styles.bottones}>
+           <button>Borrar</button>
+           <button>Informar</button>
+           <button>Donwload</button>
+          </div>
+        </div>
+        <div className={styles.pedidoFactura}>
+         <div className={styles.encabezado}>
+          <p>Pedido Numero #</p>
+          <p>Fecha:</p>
+          <p>Hora:</p>
+         </div>
+          <div className={styles.contenido}>
+            <p>Profesor:</p>
+            <p>Cruso:</p>
+            <p>Divicion:</p>
+          </div>
+          <div className={styles.librosPedido}>
+
+          </div>
+          <div className={styles.bottones}>
+           <button>Borrar</button>
+           <button>Informar</button>
+           <button>Donwload</button>
+          </div>
+        </div>
+        <div className={styles.pedidoFactura}>
+         <div className={styles.encabezado}>
+          <p>Pedido Numero #</p>
+          <p>Fecha:</p>
+          <p>Hora:</p>
+         </div>
+          <div className={styles.contenido}>
+            <p>Profesor:</p>
+            <p>Cruso:</p>
+            <p>Divicion:</p>
+          </div>
+          <div className={styles.librosPedido}>
+
+          </div>
+          <div className={styles.bottones}>
+           <button>Borrar</button>
+           <button>Informar</button>
+           <button>Donwload</button>
+          </div>
+        </div>
        </div>
       </>
     )
