2be7d7c7f768a613e28e158caa59b8e7a0647b76
TJMolina <tobiasmolina12manchitas@gmail.com>
2023-11-20 22:45:47 -0300

    stoc

--- src/pages/Stock.jsx
+++ src/pages/Stock.jsx
@@ -1,31 +1,26 @@
 //Components
-import { useState } from "react"
-import Header from "../components/Header"
-import StockItem from "../components/StockItem"
+import { useState } from "react";
+import Header from "../components/Header";
+import StockItem from "../components/StockItem";
 //Styles
-import styles from "../modules/Stock.module.css"
-import { getLibros } from "../utils/manejarLibros";
+import styles from "../modules/Stock.module.css";
+import { useLibrosContext } from "../context/libroContexts";
 
 function Stock() {
-  const [libros, setLibros] = useState([]);
-  useState(() => {
-    getLibros(setLibros);
-
-  }, []);
+  const { libros } = useLibrosContext();
   return (
     <>
       {/*Stock*/}
       <Header />
       <div className={styles.stockContent}>
-        {
-          libros.length > 0 ?
-            libros.map(libro => <StockItem key={libro._id} libro={libro}/>)
-            :
-            <div>No hay libros aun</div>
-        }
+        {libros.length > 0 ? (
+          libros.map((libro) => <StockItem key={libro._id} libro={libro} />)
+        ) : (
+          <div>No hay libros aun</div>
+        )}
       </div>
     </>
-  )
+  );
 }
 
-export default Stock
+export default Stock;
